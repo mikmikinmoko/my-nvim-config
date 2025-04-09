74bfcadaa81c3dbaec8df1b664b708bff13f4b54
mikmikinmoko <mikmikinmoko05@gmail.com>
2025-04-10 00:17:27 +0800

    added cmp

---
+++ lua/plugins/lsp-cmp.lua
@@ -0,0 +1,38 @@
+return {
+  -- Completion engine
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp", -- LSP source
+      "hrsh7th/cmp-buffer", -- Buffer words
+      "hrsh7th/cmp-path", -- File system paths
+      "saadparwaiz1/cmp_luasnip", -- Snippet source
+      "L3MON4D3/LuaSnip", -- Snippet engine
+    },
+    event = "InsertEnter",
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
+}
